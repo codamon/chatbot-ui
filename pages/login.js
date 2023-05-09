7f25e76734353a9b2d365a5bb4c1ae1108abd799
Damon <upjel.w@gmail.com>
2023-05-09 21:19:53 +1200

    rm forgot pwd and remeberme

--- pages/login.js
+++ pages/login.js
@@ -82,25 +82,25 @@ const LoginPage = () => {
                 />
               </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember_me"
-                    name="remember_me"
-                    type="checkbox"
-                    className="h-4 w-4 bg-blue-500 focus:ring-blue-400 border-gray-300 rounded"
-                    onClick={() => onRememberClick}
-                  />
-                  <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-800">
-                    Remember me
-                  </label>
-                </div>
-                <div className="text-sm">
-                  <a href="#" className="font-medium text-blue-500 hover:text-blue-600">
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
+              {/*<div className="flex items-center justify-between">*/}
+              {/*  <div className="flex items-center">*/}
+              {/*    <input*/}
+              {/*      id="remember_me"*/}
+              {/*      name="remember_me"*/}
+              {/*      type="checkbox"*/}
+              {/*      className="h-4 w-4 bg-blue-500 focus:ring-blue-400 border-gray-300 rounded"*/}
+              {/*      onClick={() => onRememberClick}*/}
+              {/*    />*/}
+              {/*    <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-800">*/}
+              {/*      Remember me*/}
+              {/*    </label>*/}
+              {/*  </div>*/}
+              {/*  <div className="text-sm">*/}
+              {/*    <a href="#" className="font-medium text-blue-500 hover:text-blue-600">*/}
+              {/*      Forgot your password?*/}
+              {/*    </a>*/}
+              {/*  </div>*/}
+              {/*</div>*/}
 
               <button
                 type="submit"
