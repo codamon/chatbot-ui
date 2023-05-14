29761e5f49388e47c84465bbc6034f80eabd757a
Damon <upjel.w@gmail.com>
2023-05-14 13:17:54 +1200

    refresh token

--- utils/app/apiClient.ts
+++ utils/app/apiClient.ts
@@ -47,51 +47,23 @@ apiClient.interceptors.response.use(
         return response;
     },
     async (error: AxiosError) => {
-        if (!error.config) {
-            return Promise.reject(error);
-        }
-
-        const originalRequest = error.config as ExtendedAxiosRequestConfig;
-
         // 如果响应为 401（未授权），尝试刷新 access_token
-        if (error.response?.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true;
 
-            try {
-                // 获取刷新令牌
-                const refreshToken = localStorage.getItem('refresh_token');
-
-                if (!refreshToken) {
-                    throw new Error('Refresh token not found');
-                }
-
-                // 请求刷新 access_token
-                const response = await axios.post(process.env.NEXT_PUBLIC_API_URL + '/api/auth/refresh', {
-                    refresh_token: refreshToken,
-                });
-
-                console.log('--66:', response.data);
-                // 存储新的 access_token 并重试原始请求
-                localStorage.setItem('access_token', response.data.access_token);
-
-                if (originalRequest.headers) {
-                    originalRequest.headers.Authorization = `Bearer ${response.data.access_token}`;
-                } else {
-                    originalRequest.headers = { Authorization: `Bearer ${response.data.access_token}` };
-                }
-
-                return apiClient(originalRequest);
-            } catch (error) {
+        switch (error.response?.status) {
+            // 如果响应中的 http code 为 401，那么则此用户可能 token 失效了之类的，我会触发 logout 方法，清除本地的数据并将用户重定向至登录页面
+            case 401:
                 console.log('刷新令牌失败，清除存储并跳转到登录页面');
                 // 刷新令牌失败，清除存储并跳转到登录页面
                 localStorage.removeItem('access_token');
                 localStorage.removeItem('refresh_token');
                 // 重定向到登录页面
                 window.location.href = '/login';
-            }
+                break;
+            // 如果响应中的 http code 为 400，那么就弹出一条错误提示给用户
+            case 400:
+                console.error('400 error'); // 你可以根据你的应用替换这里的错误处理逻辑
+                break;
         }
-
-        // 其他错误，直接返回
         return Promise.reject(error);
     },
 );
