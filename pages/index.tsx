74d29c732d0efe6dbbaef9e24662cd4430d24318
Damon <upjel.w@gmail.com>
2023-05-14 14:11:18 +1200

    server

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ interface HomeProps {
 }
 
 const Home: React.FC<HomeProps> = ({
-                                       serverSideApiKeyIsSet,
+                                       serverSideApiKeyIsSet= true,
                                        serverSidePluginKeysSet,
                                        defaultModelId,
                                    }) => {
